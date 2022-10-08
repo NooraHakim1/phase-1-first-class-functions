972487918b43c2f0adb077cb7a99063b5755dad2
Noora <you@example.comnoorahakim14@gmail.com>
2022-10-08 15:09:16 +0300

    assignment completed

---
+++ index.js
@@ -0,0 +1,15 @@
+function receivesAFunction(arg){
+    arg();
+    return 'anything';
+}
+function returnsANamedFunction(){
+    return sayHi;
+}
+function sayHi(){
+}
+
+function returnsAnAnonymousFunction(){
+    return function(){
+        
+    };
+}
